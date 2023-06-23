c9c10f198d36621078e942968c14658769b107fc
Tristan Bierig <trista.bierig@gmail.com>
2023-06-23 13:32:47 +0200

    sidebarfocus togglefunction

--- js/script.js
+++ js/script.js
@@ -1,5 +1,8 @@
+let sidebarFocus;
+
 async function init() {
     await includeHTML();
+    toggleSidebarFocus();
 }
 
 
@@ -7,7 +10,7 @@ async function includeHTML() {
     let includeElements = document.querySelectorAll('[w3-include-html]');
     for (let i = 0; i < includeElements.length; i++) {
         const element = includeElements[i];
-        file = element.getAttribute("w3-include-html"); // "includes/header.html"
+        file = element.getAttribute("w3-include-html");
         let resp = await fetch(file);
         if (resp.ok) {
             element.innerHTML = await resp.text();
@@ -15,4 +18,14 @@ async function includeHTML() {
             element.innerHTML = 'Page not found';
         }
     }
+}
+
+
+function toggleSidebarFocus(i) {
+    if (i !== undefined) {
+        sidebarFocus = i;
+        sessionStorage.setItem("sidebarFocus", i)
+    }
+    let focus = sessionStorage.getItem("sidebarFocus");
+    document.getElementById('sidebar' + focus).classList.add('sidebar-focus');
 }
