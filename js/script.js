f59dec43e1810307fbb0c881987ff18fecbee7bf
BayerTobias <tbayer2@gmx.de>
2023-06-24 07:50:58 +0200

    merge test

--- js/script.js
+++ js/script.js
@@ -1,31 +1,29 @@
 let sidebarFocus;
 
 async function init() {
-    await includeHTML();
-    toggleSidebarFocus();
+  await includeHTML();
+  toggleSidebarFocus();
 }
-
 
 async function includeHTML() {
-    let includeElements = document.querySelectorAll('[w3-include-html]');
-    for (let i = 0; i < includeElements.length; i++) {
-        const element = includeElements[i];
-        file = element.getAttribute("w3-include-html");
-        let resp = await fetch(file);
-        if (resp.ok) {
-            element.innerHTML = await resp.text();
-        } else {
-            element.innerHTML = 'Page not found';
-        }
+  let includeElements = document.querySelectorAll("[w3-include-html]");
+  for (let i = 0; i < includeElements.length; i++) {
+    const element = includeElements[i];
+    file = element.getAttribute("w3-include-html");
+    let resp = await fetch(file);
+    if (resp.ok) {
+      element.innerHTML = await resp.text();
+    } else {
+      element.innerHTML = "Page not found";
     }
+  }
 }
 
-
 function toggleSidebarFocus(i) {
-    if (i !== undefined) {
-        sidebarFocus = i;
-        sessionStorage.setItem("sidebarFocus", i)
-    }
-    let focus = sessionStorage.getItem("sidebarFocus");
-    document.getElementById('sidebar' + focus).classList.add('sidebar-focus');
+  if (i !== undefined) {
+    sidebarFocus = i;
+    sessionStorage.setItem("sidebarFocus", i);
+  }
+  let focus = sessionStorage.getItem("sidebarFocus");
+  document.getElementById("sidebar" + focus).classList.add("sidebar-focus");
 }
