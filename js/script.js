346b925c53ba1bdbc52e292477a60978d1ed8b13
BayerTobias <tbayer2@gmx.de>
2023-06-25 16:05:51 +0200

    sidebar bugfix

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@ async function init() {
   await includeHTML();
 }
 
-
 async function includeHTML() {
   let includeElements = document.querySelectorAll("[w3-include-html]");
   for (let i = 0; i < includeElements.length; i++) {
@@ -17,7 +16,6 @@ async function includeHTML() {
   }
 }
 
-
 /**
  * Prevents onclick functions to fire when an upper clickfunction gets triggerd
  *
@@ -27,50 +25,59 @@ function doNotClose(event) {
   event.stopPropagation();
 }
 
-
 /**
  * This function highlights the currently open page in the sidebar navigation
  *
  */
 function toggleSidebarFocus() {
   const pathName = window.location.pathname;
-  let idClass = 'sidebar-' + pathName.replace('/html/', '').replace('.html', '').replace('/join', '');
-  let idObject = 'sidebar-object-' + pathName.replace('/html/', '').replace('.html', '').replace('/join', '');
+  let idClass =
+    "sidebar-" +
+    pathName
+      .replace("/html/", "")
+      .replace(".html", "")
+      .replace("/join", "")
+      .replace("/Modul_10", "");
+  let idObject =
+    "sidebar-object-" +
+    pathName
+      .replace("/html/", "")
+      .replace(".html", "")
+      .replace("/join", "")
+      .replace("/Modul_10", "");
 
   changeSvgColor(idObject);
-  document.getElementById(idClass).classList.add('sidebar-focus');
+  document.getElementById(idClass).classList.add("sidebar-focus");
 }
-
 
 /**
  * This function changes the SVG-Icon of the current page sidebartab to color white
- * 
- * @param {string} idObject - This is the object for the currently highlighted SVG 
+ *
+ * @param {string} idObject - This is the object for the currently highlighted SVG
  */
 function changeSvgColor(idObject) {
   let object = document.getElementById(idObject);
-  let path = object.contentDocument.getElementsByTagName('path');
-  let circle = object.contentDocument.getElementsByTagName('circle');
+  let path = object.contentDocument.getElementsByTagName("path");
+  let circle = object.contentDocument.getElementsByTagName("circle");
   if (path) {
     for (let i = 0; i < path.length; i++) {
       const element = path[i];
-      element.style.fill = '#ffffff';
+      element.style.fill = "#ffffff";
     }
   }
   if (circle.length !== 0) {
     for (let i = 0; i < circle.length; i++) {
       const element = circle[i];
-      element.style.stroke = '#ffffff';
+      element.style.stroke = "#ffffff";
     }
-    path[0].style.stroke = '#ffffff';
+    path[0].style.stroke = "#ffffff";
   }
 }
-
 
 /**
  * Toggles the Modal css class for the log out button
  *
  */
 function toggleLogOutModal() {
-  document.getElementById('log-out-modal-wrapper').classList.toggle('d-none');
+  document.getElementById("log-out-modal-wrapper").classList.toggle("d-none");
 }
