a913fc6d396a1b7bc493b25eabedfbe7576f5ba0
BayerTobias <tbayer2@gmx.de>
2023-06-25 13:17:58 +0200

    bord js code commented

--- js/bord.js
+++ js/bord.js
@@ -21,6 +21,10 @@ let testTodos = [
   },
 ];
 
+/**
+ * this function renders tasks on the bord
+ *
+ */
 function renderTodos() {
   resetDropAreas();
 
@@ -32,17 +36,34 @@ function renderTodos() {
   }
 }
 
+/**
+ * this function resets the inner HTML of all drop areas
+ *
+ */
 function resetDropAreas() {
-  const todoArea = document.getElementById("to-do-area");
-  todoArea.innerHTML = "";
-  const inProgressArea = document.getElementById("in-progress-area");
-  inProgressArea.innerHTML = "";
-  const feedbackArea = document.getElementById("awaiting-feedback-area");
-  feedbackArea.innerHTML = "";
-  const doneArea = document.getElementById("done-area");
-  doneArea.innerHTML = "";
+  const areas = [
+    "to-do-area",
+    "in-progress-area",
+    "awaiting-feedback-area",
+    "done-area",
+  ];
+
+  for (let i = 0; i < areas.length; i++) {
+    const areaId = areas[i];
+
+    const area = document.getElementById(areaId);
+    area.innerHTML = "";
+  }
 }
 
+/**
+ * this function returns HTML Code for the task cards
+ *
+ * FUNKTION NOCH IN HTML JS AULSGERN!!!!!!!!!!!!!!!!
+ *
+ * @param {object} todo JSON with task Data
+ * @returns HTML Code with data from the @todo object
+ */
 function getTodoHTML(todo) {
   return /*html*/ `
     <div draggable="true" ondragstart="startDragging(${todo.id})">
@@ -52,21 +73,48 @@ function getTodoHTML(todo) {
   `;
 }
 
+/* ===== DRAG FUNCTIONS =====*/
+
+/**
+ * this function sets the variable currentlyDraggedElement to the value of @id
+ *
+ * @param {number} id id of the dragged Element
+ */
 function startDragging(id) {
   currentlyDraggedElement = id;
 }
 
+/**
+ * this function changes the category of the dragged task to @category
+ * then removes the hover effect, resets the variable currentlyDraggedElement
+ *
+ * @param {string} category category of the target area
+ */
 function moveTo(category) {
   testTodos[currentlyDraggedElement].category = category;
   toggleDropareaHoverEffect(category + "-area", "remove");
+  currentlyDraggedElement = null;
   renderTodos();
 }
 
+/**
+ * this function allows to drop element and toogles hover effect
+ *
+ * @param {Event} ev drag event
+ * @param {string} id id of the target area
+ */
 function allowDrop(ev, id) {
   ev.preventDefault();
+  console.log(ev);
   toggleDropareaHoverEffect(id, "add");
 }
 
+/**
+ * this function adds or removes hover effect on drag areas
+ *
+ * @param {string} id id of the target area
+ * @param {string} action add or remove
+ */
 function toggleDropareaHoverEffect(id, action) {
   const dragArea = document.getElementById(id);
   if (action == "remove") {
@@ -75,6 +123,13 @@ function toggleDropareaHoverEffect(id, action) {
   if (action == "add") {
     dragArea.classList.add("dragarea-hover");
   }
+}
+
+function searchTask() {
+  const input = document.getElementById("search-task-input").value;
+  console.log(input);
+
+  /* TODO SUCHFUNKTION*/
 }
 
 renderTodos();
