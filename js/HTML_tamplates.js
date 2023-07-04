658d2e8fba36c2a373591583a0fb78e3eaff7611
Tristan Bierig <trista.bierig@gmail.com>
2023-07-04 12:13:55 +0200

    contact list design

--- js/HTML_tamplates.js
+++ js/HTML_tamplates.js
@@ -91,3 +91,50 @@ function getSubtaskHTML(subtask) {
     <div>${subtask.name}</div>
   `;
 }
+
+
+function currentContactHTML(contact) {
+  return `
+  <div>
+        <div class="img-wrapper" style="background-color: ${contact.color}">
+            ${contact.initials} 
+        </div>
+    <div>
+      <h3>${contact.name}</h3>
+      <span>
+        <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <g id="Capa 1">
+            <g id="Group 11">
+              <path id="Vector 13" d="M9 1.5V16.5" stroke="#2A3647" stroke-width="2" stroke-linecap="round" />
+              <path id="Vector 14" d="M16.5 9.1416L1.5 9.1416" stroke="#2A3647" stroke-width="2"
+                stroke-linecap="round" />
+            </g>
+          </g>
+        </svg>
+        Add Task
+      </span>
+    </div>
+  </div>
+  <div>
+    <span>Contact Information</span>
+    <span>
+      <svg width="21" height="30" viewBox="0 0 21 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path
+          d="M2.87121 22.0156L7.69054 24.9405L20.3337 4.10842C20.6203 3.63628 20.4698 3.02125 19.9977 2.73471L16.8881 0.847482C16.4159 0.56094 15.8009 0.711391 15.5144 1.18353L2.87121 22.0156Z"
+          fill="#2A3647" />
+        <path d="M2.28614 22.9794L7.10547 25.9043L2.37685 28.1892L2.28614 22.9794Z" fill="#2A3647" />
+      </svg>
+      Edit Contact
+    </span>
+  </div>
+  <div>
+    <div class="bold">Email</div>
+    <a href="mailto:${contact.email}">${contact.email}</a>
+  </div>
+  <div>
+    <div class="bold">Phone</div>
+    <a href="tel:${contact.phone}">${contact.phone}</a>
+  </div>
+
+  `;
+}
--- js/contact.js
+++ js/contact.js
@@ -27,9 +27,10 @@ function renderContacts() {
     for (let j = 0; j < letters.length; j++) {
         const element = letters[j];
         contactsList.innerHTML += /*html*/ `
-          <span class="seperator-list">${element}</span>
-          <div class="line-list"></div>
-          <div id="contact${element}" class="contact-wrapper"></div>
+            <div id="contact${element}" class="contact-wrapper">
+                <span class="seperator-list">${element}</span>
+                <div class="line-list"></div>
+            </div>
       `;
     }
 
@@ -37,16 +38,28 @@ function renderContacts() {
         const element = currentUser.contacts[k];
         let firstLetter = element.name.charAt(0);
         document.getElementById('contact' + firstLetter).innerHTML += /*html*/ `
-            <div>
-                <div>
-        
+            <div class="contact-container" id="contact${k}" onclick="showContact(${k})">
+                <div class="contact-avatar" style="background-color: ${element.color}">
+                    <span>${element.initials}</span>
                 </div>
-                <div>
-                    <span>${element.name}</span>
-                    <span>${element.email}</span>
+                <div class="contactlist-data">
+                    <span class="contact-name">${element.name}</span>
+                    <span class="contact-email">${element.email}</span>
                 </div>
             </div>
         `;
+    }
+}
+
+
+function getInitialsContact(name) {
+    let first = name.charAt(0);
+    let secondIndex = name.indexOf(' ');
+    if (secondIndex != -1) {
+        let second = name.charAt(secondIndex + 1);
+        return first.toUpperCase() + second.toUpperCase();
+    } else {
+        return first.toUpperCase();
     }
 }
 
@@ -58,12 +71,30 @@ function toggleContactModal() {
 
 
 function addNewContact() {
+    let name = contactName.value;
     currentUser.contacts.push({
-        'name': contactName.value,
+        'name': name,
         'email': contactEmail.value,
-        'phone': contactPhone.value
+        'phone': contactPhone.value,
+        'initials': getInitialsContact(name),
+        'color': getRandomColor()
     });
     setItem('users', JSON.stringify(users));
     addContactForm.reset();
     toggleContactModal();
+    renderContacts();
+}
+
+
+function showContact(i) {
+    let contact = currentUser.contacts[i];
+    let focus = document.getElementById('contact' + i);
+    currentContact.innerHTML = '';
+    currentContact.innerHTML = currentContactHTML(contact);
+
+    for (let j = 0; j < currentUser.contacts.length; j++) {
+        const element = document.getElementById('contact' + j);
+        element.classList.remove('contact-container-focus');
+    }
+    focus.classList.add('contact-container-focus');
 }
