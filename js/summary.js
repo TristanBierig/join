7b785828e6774211b8a2c7625a62357ee4524f4b
Julia Rohrer <julia-rohrer@web.de>
2023-07-08 11:32:07 +0200

    help and legal responsive

--- js/summary.js
+++ js/summary.js
@@ -1,14 +1,18 @@
+/**
+ * this function changes the greeting depending on the time of day
+ *
+ */
 function greet() {
-    let currentTime = new Date();
-    let currentHour = currentTime.getHours();
-    let greetingElement = document.getElementById("greetings");
+  let currentTime = new Date();
+  let currentHour = currentTime.getHours();
+  let greetingElement = document.getElementById("greetings");
 
-    if (currentHour < 12) {
-      greetingElement.textContent = "Good morning, ";
-    } else if (currentHour < 18) {
-      greetingElement.textContent = "Good afternoon, ";
-    } else {
-      greetingElement.textContent = "Good evening, ";
-    }
+  if (currentHour < 12) {
+    greetingElement.textContent = "Good morning, ";
+  } else if (currentHour < 18) {
+    greetingElement.textContent = "Good afternoon, ";
+  } else {
+    greetingElement.textContent = "Good evening, ";
   }
-  greet();
+}
+greet();
