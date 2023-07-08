ee839fd8927f67264d19720ce34235f9c4246023
Tristan Bierig <trista.bierig@gmail.com>
2023-07-08 21:16:20 +0200

    add task in contact error

--- js/HTML_tamplates.js
+++ js/HTML_tamplates.js
@@ -100,7 +100,7 @@ function currentContactHTML(contact, i) {
         </div>
     <div>
       <h3>${contact.name}</h3>
-      <span>
+      <span  onclick="openAddTaskOverlay()">
         <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
           <g id="Capa 1">
             <g id="Group 11">
