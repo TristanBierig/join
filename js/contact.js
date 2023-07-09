7880b45a586b45321b03b60ec5e6522ae476bf61
Tristan Bierig <trista.bierig@gmail.com>
2023-07-09 12:56:17 +0200

    bugfix sidebar change of svg implementaion

--- js/contact.js
+++ js/contact.js
@@ -11,6 +11,7 @@ async function initContacts() {
     }
     loadProfilePicture();
     renderContacts();
+    toggleSidebarFocus();
 }
 
 
--- js/script.js
+++ js/script.js
@@ -13,6 +13,7 @@ async function init() {
     return;
   }
   loadProfilePicture();
+  toggleSidebarFocus();
 }
 
 
@@ -123,33 +124,11 @@ function toggleSidebarFocus() {
   if (idClass === "sidebar-help") {
     return;
   }
-  changeSvgColor(idObject);
+  
+  document.getElementById(idObject).src = '../assets/img/icons/' + idClass + '-focus.svg';
   document.getElementById(idClass).classList.add("sidebar-focus");
 }
 
-/**
- * This function changes the SVG-Icon of the current page sidebartab to color white
- *
- * @param {string} idObject - This is the object for the currently highlighted SVG
- */
-function changeSvgColor(idObject) {
-  let object = document.getElementById(idObject);
-  let path = object.contentDocument.getElementsByTagName("path");
-  let circle = object.contentDocument.getElementsByTagName("circle");
-  if (path) {
-    for (let i = 0; i < path.length; i++) {
-      const element = path[i];
-      element.style.fill = "#ffffff";
-    }
-  }
-  if (circle.length !== 0) {
-    for (let i = 0; i < circle.length; i++) {
-      const element = circle[i];
-      element.style.stroke = "#ffffff";
-    }
-    path[0].style.stroke = "#ffffff";
-  }
-}
 
 /**
  * Toggles the Modal css class for the log out button
